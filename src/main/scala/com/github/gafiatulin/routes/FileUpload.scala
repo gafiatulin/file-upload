d8a082e7e97d1b77a1f4e555957bdf6a41fd47ee
Victor <gafiatulin@gmail.com>
2016-08-26 15:33:18 +0400

    fix fileUpload routes

--- src/main/scala/com/github/gafiatulin/routes/FileUpload.scala
+++ src/main/scala/com/github/gafiatulin/routes/FileUpload.scala
@@ -2,7 +2,7 @@ package com.github.gafiatulin.routes
 
 import akka.event.Logging
 import akka.http.scaladsl.marshalling.Marshal
-import akka.http.scaladsl.model.headers.{ByteRange, Range, RangeUnits, `Content-Range`}
+import akka.http.scaladsl.model.headers.{RangeUnits, `Content-Range`}
 import akka.http.scaladsl.model.{ContentRange, HttpResponse, RequestEntity, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
@@ -16,15 +16,23 @@ import scala.concurrent.Future
   */
 trait FileUpload extends FileMetaUtils with RoutingUtils {
 
-  private def handleCorrectEntity(adapter: LocalFileStorageAdapter, token: String, ent: RequestEntity) = optionalHeaderValueByType[`Content-Range`](){
-    case Some(`Content-Range`(RangeUnits.Bytes, ContentRange.Unsatisfiable(fileSize))) =>
+  private def handleEmptyEntity(adapter: LocalFileStorageAdapter, token: String) = headerValueByType[`Content-Range`](){
+    case `Content-Range`(RangeUnits.Bytes, ContentRange.Unsatisfiable(_)) =>
       val resp = adapter.uploaded(token)
         .flatMap{
-          case Some((last, _)) => Future.successful(HttpResponse(headers = List(Range(ByteRange(0, last)))))
+          case Some((last, _)) =>
+            val headers = if (last == 0) {List()} else {List(`Content-Range`(RangeUnits.Bytes, ContentRange.Default(0, last -1, None)))}
+            Future.successful(HttpResponse(headers = headers))
           case None => Marshal(StatusCodes.BadRequest -> "Specified uploadToken is incorrect or already completed").to[HttpResponse]
         }
         .recoverWith(recoverAfterException)
       complete(resp)
+    case _ =>
+      val resp = Marshal(StatusCodes.BadRequest -> "Inconsistent Content-Range header value").to[HttpResponse]
+      complete(resp)
+  }
+
+  private def handleEntity(adapter: LocalFileStorageAdapter, token: String, ent: RequestEntity) = optionalHeaderValueByType[`Content-Range`](){
     case Some(`Content-Range`(RangeUnits.Bytes, ContentRange.Default(first, _, _))) =>
       val resp = adapter.upload(token, ent.dataBytes, ent.contentType.mediaType, first)
         .flatMap(completeFileUpload)
@@ -43,8 +51,8 @@ trait FileUpload extends FileMetaUtils with RoutingUtils {
   }
 
   private def handleUpload(adapter: LocalFileStorageAdapter, token: String): Route = extractRequestEntity{
-    case ent: RequestEntity if ent.isKnownEmpty || safeMediaRanges.exists(_.matches(ent.contentType.mediaType)) =>
-      handleCorrectEntity(adapter, token, ent)
+    case ent: RequestEntity if ent.isKnownEmpty => handleEmptyEntity(adapter, token)
+    case ent: RequestEntity if safeMediaRanges.exists(_.matches(ent.contentType.mediaType)) => handleEntity(adapter, token, ent)
     case ent: RequestEntity =>
       val resp = Marshal((
         StatusCodes.UnsupportedMediaType,
